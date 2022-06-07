a057f0dae43107e4645b6fb10981578749b947ed
Alberto Piñeiro <a21albertopr@iessanclemente.net>
2022-06-07 17:09:44 +0200

    xerouse o código estándar da clase ContadorAscendente

---
+++ src/main/java/com/mycompany/proxectocontadorascendente/ContadorAscendente.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proxectocontadorascendente;
+
+/**
+ *
+ * @author Alberto
+ */
+public class ContadorAscendente {
+    
+        private int valorActual;
+
+    public ContadorAscendente(int valorActual) {
+        this.valorActual = valorActual;
+    }
+
+    @Override
+    public String toString() {
+        return "ContadorAscendente{" + '}';
+ 
+        
+    public int getValorActual() {
+        return valorActual;
+    }
+
+    public void setValorActual(int valorActual) {
+        this.valorActual = valorActual;
+    }
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
